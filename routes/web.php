5a563633ebd3eaaa2002eb423369cb5ef1547be0
marcellaav <patriciamarcellaav@gmail.com>
2024-05-08 01:16:41 +0700

    route sendEmail

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ChangePassCustController;
 
 Route::get('/', function () {
     return view('/admin/navbarAdminDashboard');
 });
+
+Route::post('/send-change-link', [ChangePassCustController::class, 'sendEmailCust']);
