2c2645f54cd990d24a34a0973752af8381829737
william Juvent <williamjuvent6@gmail.com>
2024-10-02 22:36:11 +0700

    lastButNotLeast

--- app/api/index.php
+++ app/api/index.php
@@ -1,2 +1,6 @@
 <?php
-    require __DIR__.'/../public/index.php';
+// Inisialisasi file dari public
+require __DIR__.'/../public/index.php';
+
+// Buat respons manual untuk serverless function
+echo "Hello from Vercel API!";
